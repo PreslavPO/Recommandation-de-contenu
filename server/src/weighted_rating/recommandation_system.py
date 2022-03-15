f228e088bd22d6ed59f186f1dd27c797fcd29f1a
Preslav TENEV <seiakimitsu@gmail.com>
2022-03-15 13:58:31 +0100

    Correction du content-based filtering

--- server/src/weighted_rating/recommandation_system.py
+++ server/src/weighted_rating/recommandation_system.py
@@ -1,13 +1,18 @@
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
 
 movies_metadata = pd.read_csv("../../Data/movies_metadata.csv", low_memory=False)
+ratings = pd.read_csv("../../Data/ratings.csv", low_memory=False)
+links = pd.read_csv("../../Data/links.csv", low_memory=False)
 # print(movies_metadata.head(3))
 # movies_metadata.info()
 
 # Calculer la moyenne de la colonne vote_average
-C = movies_metadata['vote_average'].mean()
+#C = movies_metadata['vote_average'].mean()
 #print(C)
 
 # calcule le nombre de vote minimum pour que le film soit comptabilisé, m
@@ -15,24 +20,24 @@ m = movies_metadata['vote_count'].quantile(0.90)
 #print(m)
 
 # Creer un nouveau DataSet avec uniquement les films qui ont assez de vote
-q_movies = movies_metadata.copy().loc[movies_metadata['vote_count'] >= m]
+#q_movies = movies_metadata.copy().loc[movies_metadata['vote_count'] >= m]
 #print(q_movies.shape)
 
 #print(movies_metadata.shape)
 
-def weighted_rating(x, m=m, C=C):
-    v = x['vote_count']
-    R = x['vote_average']
-    # Calcule avec la formule IMDB
-    return (v/(v+m) * R) + (m/(m+v) * C)
+#def weighted_rating(x, m=m, C=C):
+#    v = x['vote_count']
+#    R = x['vote_average']
+#    # Calcule avec la formule IMDB
+#    return (v/(v+m) * R) + (m/(m+v) * C)
 
 # Creer une nouvelle colonne score calculer a l'aide de la fonction weighted_rating
-q_movies['score'] = q_movies.apply(weighted_rating, axis=1)
+#q_movies['score'] = q_movies.apply(weighted_rating, axis=1)
 
 #print(q_movies.shape)
 
 #Trie les films en fonction du score par ordre decroissant
-q_movies = q_movies.sort_values('score', ascending=False)
+#q_movies = q_movies.sort_values('score', ascending=False)
 
 #Affiche top 20 films
 #print(q_movies[['title', 'vote_count', 'vote_average', 'score']].head(20))
@@ -48,23 +53,33 @@ q_movies = q_movies.sort_values('score', ascending=False)
 # Le stop_words supprime tous les mots anglais inutiles comme 'the', 'a' ...
 tfidf = TfidfVectorizer(stop_words='english')
 
+#Supprime les films avec peu de votes
+movies_metadata = movies_metadata.copy().loc[movies_metadata['vote_count'] >= m]
+
+#Relet les index à jour comme on a supprimé des films
+movies_metadata = movies_metadata.reset_index()
+
 #overview = synopsis
 #Remplace les synopsis inexistants par '' car sinon ça fait des erreurs
-q_movies['overview'] = q_movies['overview'].fillna('')
+movies_metadata['overview'] = movies_metadata['overview'].fillna('')
 
 #Crée la matrice qui contient les films et les mots qui apparaissent
-tfidf_matrix = tfidf.fit_transform(q_movies['overview'])
+tfidf_matrix = tfidf.fit_transform(movies_metadata['overview'])
 
 # Algorithme pour calculer la similarité entre les films en fonction dees mots (cosine)
 cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
 
 #Crée une map qui associe l'index au titre d'un film, on en a besoin pour la fonction get_recommandations
-indices = pd.Series(q_movies.index, index=q_movies['title']).drop_duplicates()
+indices = pd.Series(movies_metadata.index, index=movies_metadata['title']).drop_duplicates()
 
 # Fonction qui prend un titre de film en entrée et resort les 10 fillms avec le plus de similarités dans le synopsis
 def get_recommendations(title, cosine_sim=cosine_sim):
-    #Récupère l'index du film
-    idx = indices[title]
+    #Récupère l'index du film et prend uniquement le premier film avec ce titre
+    temp = indices[title]
+    if(temp.size > 1):
+        idx = temp[0]
+    else :
+        idx = temp
 
     # Récupère les scores de similarité de tous les autres films avec celui-là
     sim_scores = list(enumerate(cosine_sim[idx]))
@@ -79,6 +94,13 @@ def get_recommendations(title, cosine_sim=cosine_sim):
     movie_indices = [i[0] for i in sim_scores]
 
     # Retourne les 10 films
-    return q_movies['title'].iloc[movie_indices]
+    return movies_metadata['title'].iloc[movie_indices]
 
-print(get_recommendations('Batman Forever'))
+print(get_recommendations('The Dark Knight Rises'))
+
+
+
+
+
+
+
